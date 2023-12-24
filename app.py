c2c8e22db622eac09b18ee821a98bc1353046e77
kyleleey <zzli@zju.edu.cn>
2023-12-24 04:36:01 -0800

    update

--- app.py
+++ app.py
@@ -10,15 +10,8 @@ import torch
 if os.getenv('SYSTEM') == 'spaces':
     os.system('pip install --global-option="--no-networks" git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch')
 
-    pyt_version_str=torch.__version__.split("+")[0].replace(".", "")
-    version_str="".join([
-        f"py3{sys.version_info.minor}_cu",
-        torch.version.cuda.replace(".",""),
-        f"_pyt{pyt_version_str}"
-    ])
     os.system('pip install fvcore iopath')
-    os.system(f'pip install --no-index --no-cache-dir pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/{version_str}/download.html')
-    # os.system('pip install "git+https://github.com/facebookresearch/pytorch3d.git@stable"')
+    os.system('pip install "git+https://github.com/facebookresearch/pytorch3d.git"')
 
 import cv2
 import time
